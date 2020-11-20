cad9c33597aad5dc4e0bd608b06b7dbabcfda9f6
Vadim Chursin <vadchurs@mail.ru>
2020-11-20 16:56:23 +0300

    Updates

---
+++ precache-manifest.cdfb1236d4a3e5884f689dddcd004d79.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e69b86fa22b2820d0db51ae6ebf71af",
+    "url": "://churs90.github.io/myPortfolio/index.html"
+  },
+  {
+    "revision": "b688d51d20c2cfb8bf1e",
+    "url": "://churs90.github.io/myPortfolio/static/css/main.dba81a1d.chunk.css"
+  },
+  {
+    "revision": "3223ec8bc30222e6b199",
+    "url": "://churs90.github.io/myPortfolio/static/js/2.34ef2c83.chunk.js"
+  },
+  {
+    "revision": "b688d51d20c2cfb8bf1e",
+    "url": "://churs90.github.io/myPortfolio/static/js/main.660093dc.chunk.js"
+  },
+  {
+    "revision": "7be317252ff1b7714d71",
+    "url": "://churs90.github.io/myPortfolio/static/js/runtime-main.8693ef48.js"
+  },
+  {
+    "revision": "bed050013c7ee969f8e9d21c130a046a",
+    "url": "://churs90.github.io/myPortfolio/static/media/cropped-volcano-brandon-project.bed05001.jpg"
+  },
+  {
+    "revision": "3b9a5fbc8f954e9558e86e03eb4e1feb",
+    "url": "://churs90.github.io/myPortfolio/static/media/maxresdefault.3b9a5fbc.jpg"
+  },
+  {
+    "revision": "3e497f21d2b3cce8fdb2d14e35364805",
+    "url": "://churs90.github.io/myPortfolio/static/media/todo-list-850x476.3e497f21.jpg"
+  },
+  {
+    "revision": "cab05f6a618c9f4966a414ab631c3fc0",
+    "url": "://churs90.github.io/myPortfolio/static/media/vad.cab05f6a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "://churs90.github.io/portfolio/precache-manifest.3c47412411fb11075d5cec502a880395.js"
+  "://churs90.github.io/myPortfolio/precache-manifest.cdfb1236d4a3e5884f689dddcd004d79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("://churs90.github.io/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("://churs90.github.io/myPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
