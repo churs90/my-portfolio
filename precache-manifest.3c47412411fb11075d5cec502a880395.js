cbe1e62e1560fc5591fdaf3d8fd2b296e28c8aa1
Vadim Chursin <vadchurs@mail.ru>
2020-11-20 14:37:12 +0300

    Updates

---
+++ precache-manifest.3c47412411fb11075d5cec502a880395.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70346906786e67bbc66b1b9baa259f65",
+    "url": "://churs90.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "b48d4d3ea8942f68566e",
+    "url": "://churs90.github.io/portfolio/static/css/main.dba81a1d.chunk.css"
+  },
+  {
+    "revision": "3223ec8bc30222e6b199",
+    "url": "://churs90.github.io/portfolio/static/js/2.34ef2c83.chunk.js"
+  },
+  {
+    "revision": "b48d4d3ea8942f68566e",
+    "url": "://churs90.github.io/portfolio/static/js/main.93da1ea8.chunk.js"
+  },
+  {
+    "revision": "616a17a159b12f3eb6a7",
+    "url": "://churs90.github.io/portfolio/static/js/runtime-main.f6ece6b1.js"
+  },
+  {
+    "revision": "bed050013c7ee969f8e9d21c130a046a",
+    "url": "://churs90.github.io/portfolio/static/media/cropped-volcano-brandon-project.bed05001.jpg"
+  },
+  {
+    "revision": "3b9a5fbc8f954e9558e86e03eb4e1feb",
+    "url": "://churs90.github.io/portfolio/static/media/maxresdefault.3b9a5fbc.jpg"
+  },
+  {
+    "revision": "3e497f21d2b3cce8fdb2d14e35364805",
+    "url": "://churs90.github.io/portfolio/static/media/todo-list-850x476.3e497f21.jpg"
+  },
+  {
+    "revision": "cab05f6a618c9f4966a414ab631c3fc0",
+    "url": "://churs90.github.io/portfolio/static/media/vad.cab05f6a.jpg"
+  }
+]);
